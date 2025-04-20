cbf64b29d78e58aa3254821bfa502e1c5b32506c
Brian Howald <bdhowald@gmail.com>
2025-04-20 01:38:48 -0400

    Add etag to original queries, as well.

--- server.js
+++ server.js
@@ -4202,6 +4202,15 @@ const server = http.createServer(async (req, res) => {
         getVehicleResponse(vehicle, fields, externalData)
       )
     ).then((allResponses) => {
+      const body = { data: allResponses }
+      const eTag = createHash('md5').update(JSON.stringify(body)).digest('hex')
+      res.setHeader('ETag', `"${eTag}"`)
+
+      if (req.headers['if-none-match'] === `"${eTag}"`) {
+        res.writeHead(304)
+        res.end()
+      }
+
       res.writeHead(200)
       res.end(JSON.stringify({ data: allResponses }))
     })
