b053ed03aafb10a405384949bebfd60df2440218
Brian Howald <bdhowald@gmail.com>
2025-09-12 15:12:48 -0400

    Temporary workaround for Google APIs returning 'The Bronx' instead of 'Bronx'.

--- src/services/geocodingService/index.ts
+++ src/services/geocodingService/index.ts
@@ -87,7 +87,7 @@ const getBoroughService = async (streetAddress: string | undefined, loggingKey: 
 
       potentialBorough = geocodeFromGoogle.borough
 
-      if (potentialBorough in Borough) {
+      if (potentialBorough in Borough || potentialBorough == 'The Bronx') {
         // Only insert geocode if it's for a borough.
         await insertGeocodeIntoDatabase(geocodeFromGoogle, loggingKey)
       }
